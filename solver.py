84d667c683931d1f9603a06ea7119bfb848086a4
Tarang Srivastava <tarang.sriv@berkeley.edu>
2020-12-05 20:29:20 -0500

    moved shit

--- solver.py
+++ solver.py
@@ -2,6 +2,7 @@ import networkx as nx
 from parse import read_input_file, write_output_file
 from utils import *
 import sys
+import glob
 
 
 def solve(G, s):
@@ -108,23 +109,23 @@ def solve(G, s):
 
 # Usage: python3 solver.py test.in
 
-if __name__ == '__main__':
-    assert len(sys.argv) == 2
-    path = sys.argv[1]
-    G, s = read_input_file(path)
-    D, k = solve(G, s)
-    assert is_valid_solution(D, G, s, k)
-    print("Total Happiness: {}".format(calculate_happiness(D, G)))
-    write_output_file(D, 'out/test.out')
+# if __name__ == '__main__':
+#     assert len(sys.argv) == 2
+#     path = sys.argv[1]
+#     G, s = read_input_file(path)
+#     D, k = solve(G, s)
+#     assert is_valid_solution(D, G, s, k)
+#     print("Total Happiness: {}".format(calculate_happiness(D, G)))
+#     write_output_file(D, 'out/test.out')
 
 
 # For testing a folder of inputs to create a folder of outputs, you can use glob (need to import it)
-# if __name__ == '__main__':
-#     inputs = glob.glob('file_path/inputs/*')
-#     for input_path in inputs:
-#         output_path = 'file_path/outputs/' + basename(normpath(input_path))[:-3] + '.out'
-#         G, s = read_input_file(input_path, 100)
-#         D, k = solve(G, s)
-#         assert is_valid_solution(D, G, s, k)
-#         cost_t = calculate_happiness(T)
-#         write_output_file(D, output_path)
+if __name__ == '__main__':
+    inputs = glob.glob('file_path/inputs/*')
+    for input_path in inputs:
+        output_path = 'file_path/outputs/' + basename(normpath(input_path))[:-3] + '.out'
+        G, s = read_input_file(input_path, 100)
+        D, k = solve(G, s)
+        assert is_valid_solution(D, G, s, k)
+        cost_t = calculate_happiness(T)
+        write_output_file(D, output_path)
