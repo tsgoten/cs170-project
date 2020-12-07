edcfb72eab0a28bcbc93cccded2ee3ad074bba48
vinny-sri <vinny.sri@berkeley.edu>
2020-12-06 20:02:50 -0800

    Progress on dynamic

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -26,51 +26,55 @@ def solve(G, s, output_file=''):
         return
     normalizer = 10000 / (old_happiness + 1)
     print(old_happiness)
-    def get_D(rooms):
+
+    def get_D():
         D = {}
         D = read_output_file(output_file, G, s)
         room_to_student = {}
         for k, v in D.items():
             room_to_student.setdefault(v, []).append(k)
-        rooms = len(room_to_student)
-        # for n in room_to_student[rooms - 1]:
-        #     D[n] = 0
-        # room_to_student[0] += room_to_student.pop(rooms - 1)
-        # rooms = len(room_to_student)
-        # if rooms < 1:
-        #     return D, rooms + 1
-        # D = {}
 
-        # # # RANDOM ASSIGNMENT #
-        # for n in range(students):
-        #     D[n] = random.randrange(rooms)
-
-        # room_to_student = {}
-        # for k, v in D.items():
-        #     room_to_student.setdefault(v, []).append(k)
-        # rooms = len(room_to_student)
-        # S = s / rooms
         def get_happiness():
-
             if not is_valid_solution(D, G, s, len(room_to_student)):
-                # return - is_valid_solution_stress(D, G, s, len(room_to_student))
                 return -100
             else:
                 return calculate_happiness(D, G)
 
         def swap_happiness(n1, n2):
             swap(n1, n2)
-            # r1, r2 = room_to_student[D[n1]], room_to_student[D[n2]]
-            # if calculate_stress_for_room(r1, G) > S or calculate_stress_for_room(r2, G) > S:
             happiness = get_happiness()
             swap(n1, n2)
             return happiness
+
+        def add_student(n1, room):
+            room_to_student[room].append(n1)
+            r1 = room_to_student[D[n1]]
+            i1 = r1.index(n1)
+            r1.remove(i1)
+            if i1 == 0:
+                room_to_student[D[n1]].pop()
+            D[n1] = len(room_to_student)
+            room_to_student[D[n1]] = [n1]
+
+            index = 0
+            output = {}
+            for r in room_to_student.keys():
+                output[index] = room_to_student[r]
+                index += 1
 
         def swap(n1, n2):
             r1, r2 = room_to_student[D[n1]], room_to_student[D[n2]]
             i1, i2 = r1.index(n1), r2.index(n2)
             r1[i1], r2[i2] = n2, n1
             D[n2], D[n1] = D[n1], D[n2]
+
+        def remove(n1):
+            r1 = room_to_student[D[n1]]
+            if len(r1) > 1:
+                i1 = r1.index(n1)
+                r1.remove(i1)
+                D[n1] = len(room_to_student)
+                room_to_student[D[n1]] = [n1]
 
         def maybe_swap(n1, n2, T):
             curr_hap = get_happiness() * normalizer
@@ -81,16 +85,46 @@ def solve(G, s, output_file=''):
             if r < p:
                 # print(p)
                 # print('SWAPPED!')
-                # print(room_to_student)
                 swap(n1, n2)
+
+        def maybe_add(n1, T):
+            #Random chance of adding n1 to n2
+            curr_hap = get_happiness() * normalizer
+            swap_hap = swap_happiness(n1, n2) * normalizer
+            r = random.random()
+            p = math.exp((swap_hap - curr_hap) / T)
+            # print(n1, n2, swap_hap, curr_hap, r, p)
+            if r < p:
+                # print(p)
+                # print('SWAPPED!')
+                room = random.randrange(len(room_to_student))
+                add_student(n1, room)
+        
+        def maybe_remove(n1, T):
+            curr_hap = get_happiness() * normalizer
+            swap_hap = swap_happiness(n1, n2) * normalizer
+            r = random.random()
+            p = math.exp((swap_hap - curr_hap) / T)
+            # print(n1, n2, swap_hap, curr_hap, r, p)
+            if r < p:
+                # print(p)
+                # print('SWAPPED!')
+                remove(n1)
+        
         prev, curr = 0, 0
         converged = 0
         for countdown in range(100, 0, -1):
             curr = get_happiness()
             print(curr)
             for _ in range(200):
-                n1, n2 = floor(random.randrange(students)), floor(random.randrange(students))
-                if D[n1] != D[n2]:
+                n1, n2 = floor(random.randrange(students * 1.5)), floor(random.randrange(students * 1.5))
+                if n2 > students and n1 < students:
+                    # Move student into random breakout room that exists
+                    maybe_add(n1, countdown)
+                elif n2 < students and n1 > students:
+                    # Move student into breakout room by itself
+                    maybe_remove(n2, countdown)
+                elif D[n1] != D[n2]:
                     maybe_swap(n1, n2, countdown)
             if prev == curr:
                 converged += 1
@@ -99,32 +133,8 @@ def solve(G, s, output_file=''):
                 break
         return D, rooms
 
-    max_rooms = 0
-    for i in range(1, len(old_D)):
-        if old_D[i] > max_rooms:
-            max_rooms = old_D[i]
-    max_rooms = max_rooms + 1
-    best_output = old_D
-    best_rooms = max_rooms
-    for i in range(1, max_rooms):
-        output, rooms = get_D(i) # <-----------------------------------------------------CHANGE THIS FOR THE ROOMS YOU WANT
-        print(output)
-        print(is_valid_solution(output, G, s, rooms))
-        # print(calculate_happiness(output, G))
-        if calculate_happiness(output, G) > old_happiness and is_valid_solution(output, G,s, rooms):
-            best_output = output
-            best_rooms = rooms
-    if best_output == old_D and best_rooms == max_rooms + 1:
-        return None
-    else:
-        return best_output, best_rooms
-    
 
-    # assns =  [(get_D(rooms), rooms) for rooms in range(1, floor(students))]
-    # output, rooms = max(assns, key=lambda d: calculate_happiness(d[0], G))
-    # print(calculate_happiness(output, G)) 
-    output, rooms = get_D(1) # <-----------------------------------------------------CHANGE THIS FOR THE ROOMS YOU WANT
-    # print(calculate_happiness(output, G))
+    output, rooms = get_D()
     if calculate_happiness(output, G) > old_happiness and is_valid_solution(output, G,s, rooms):
         return output, rooms
     else:
